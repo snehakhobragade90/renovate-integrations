05b5e485cc804e6727b7e31834790d43c4365b26
David Smith <dsmith@nerdwallet.com>
2020-05-16 16:30:01 -0700

    fix: initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // logLevel: "debug",
+  logFile: "renovate.log",
+  npmrc: "registry=https://artifactory.nerdwallet.io/artifactory/api/npm/npm",
+  pipenv: {
+    enabled: true,
+  },
+  postUpateOptions: ["gomodTidy"],
+  python: {
+    registryUrls: [
+      "https://artifactory.nerdwallet.io/artifactory/api/pypi/pypi-public-test/simple/",
+    ],
+  },
+  repositories: [
+    // "NerdWallet/nwpy-gunicorn",
+    // "NerdWallet/features.nerdwallet",
+    // "NerdWallet/user-data-platform",
+    // "NerdWallet/identity.nerdwallet",
+    // "NerdWallet/indy",
+    // "NerdWallet/logger.nerdwallet",
+    "NerdWallet/yak",
+  ],
+  separateMultipleMajor: true,
+  unicodeEmoji: true,
+};
