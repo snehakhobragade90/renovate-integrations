81156102fbb68f2c031f6fde84b361ea213e1b1a
Piper Chester <piperchester@gmail.com>
2020-06-30 14:43:45 -0700

    refactor: minor clean up

--- renovate.js
+++ renovate.js
@@ -1,1 +1,25 @@
-require('renovate')
+#!/usr/bin/env node
+
+/**
+ * Script that can be called to invoke Renovate
+ * with our necessary integrations. This could
+ * be possible from within config.js, but we decouple
+ * the two.
+ */
+
+const chalk = require("chalk");
+
+// Renovate requires a GitHub token, which `nwjs-app-config`
+// will automatically decrypt.
+// const config = require("@nerdwallet/app-config");
+
+// const gitHubToken = config.RENOVATE.AUTH.GITHUB_ACCESS_TOKEN;
+// console.log(gitHubToken);
+
+const renovateConfig = require('./config');
+
+console.log(renovateConfig);
+
+console.log(`${chalk.bold('Starting')} Renovate...`)
+// require('renovate');
+
