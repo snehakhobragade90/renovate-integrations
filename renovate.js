39f09a924d3c658f7341c6f39ecd08a0dd182e13
Piper Chester <piperchester@gmail.com>
2020-06-30 15:13:12 -0700

    refactor: clean up

--- renovate.js
+++ renovate.js
@@ -9,17 +9,20 @@
 
 const chalk = require("chalk");
 
+if (process.env.NODE_ENV === "test") {
+  console.log(`${chalk.bold("Testing...")}`);
+  process.exit(0);
+}
+
 // Renovate requires a GitHub token, which `nwjs-app-config`
 // will automatically decrypt.
-// const config = require("@nerdwallet/app-config");
+const config = require("@nerdwallet/app-config");
 
-// const gitHubToken = config.RENOVATE.AUTH.GITHUB_ACCESS_TOKEN;
-// console.log(gitHubToken);
+// We then configure it to be available to use before
+// invoking Renovate. For more details, see
+// https://github.com/renovatebot/renovate/blob/master/docs/development/self-hosting.md#github-enterprise
+const renovateConfig = require("./config");
+renovateConfig.token = config.RENOVATE.AUTH.GITHUB_ACCESS_TOKEN;
 
-const renovateConfig = require('./config');
-
-console.log(renovateConfig);
-
-console.log(`${chalk.bold('Starting')} Renovate...`)
-// require('renovate');
-
+console.log(`${chalk.green("Starting Renovate...")}`);
+require("renovate");
