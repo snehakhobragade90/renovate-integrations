63380d0cf6133297678390ab655166268e98aad3
David Smith <dsmith@nerdwallet.com>
2020-07-11 14:25:52 -0700

    fix: add pytest to popular Py libs

--- config.js
+++ config.js
@@ -119,6 +119,7 @@ module.exports = {
          "^python-jose",
          "^python-json-logger",
          "^python-levenshtein",
+         "^pytest",
          "^pytz",
          "^pyyaml",
          "^raven",
